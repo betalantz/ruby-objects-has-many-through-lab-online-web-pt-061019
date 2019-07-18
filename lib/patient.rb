efb0cacccd4e84bc7d16e3276fc544972c2b3abd
Lantz Warrick <betalantz@gmail.com>
2019-07-18 18:05:59 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,20 @@
+class Patient
+
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
+
+  def new_appointment(date, doctor)
+    Appointment.new(date, self, doctor)
+  end
+
+  def self.all
+    @@all
+  end
+
+end
