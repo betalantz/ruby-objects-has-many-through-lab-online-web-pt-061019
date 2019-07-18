00fbc65b130b49ecfa412a6751ac99f670a04641
Lantz Warrick <betalantz@gmail.com>
2019-07-18 18:07:16 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,12 +9,16 @@ class Patient
     self.class.all << self
   end
 
+  def self.all
+    @@all
+  end
+
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
 
-  def self.all
-    @@all
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
   end
 
 end
